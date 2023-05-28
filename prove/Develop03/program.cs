733ee0d1e76466fbc222b7ffd81b8a5bff59741f
kattbcc17 <kattbcc17@byui.edu>
2023-05-27 22:36:13 -0500

    memorize

--- prove/Develop03/program.cs
+++ prove/Develop03/program.cs
@@ -3,7 +3,7 @@ class Program{
     static void Main(string[] args){
         GetScripture script1 = new GetScripture();
         string entry1 = script1.GetFromFile();
-        Console.WriteLine($"{entry1}\n\nPress enter to fill up the blank space.");
+        Console.WriteLine($"{entry1}\n\nPress enter to hide a few words.");
         Console.ReadLine();
 
         CoverWord cover1 = new CoverWord();
