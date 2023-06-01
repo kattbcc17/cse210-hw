873f377c447f303153dda93edec3af1d2166462a
kattbcc17 <kattbcc17@byui.edu>
2023-06-01 11:48:57 -0500

    do while loop

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -11,16 +11,69 @@ class Program
         Console.WriteLine("Menu Options:");
         Console.WriteLine("1. Start breathing activity");
         Console.WriteLine("2. Start reflecting activity");
-        Console.WriteLine("3. Start listening activity");
+        Console.WriteLine("3. Start listing activity");
         Console.WriteLine("4. Quit");
-        Console.Write("Select a choise from the menu: ");
+        Console.Write("Select a choice from the menu: ");
         }
     
     //Start
     Console.Clear();
-    Console.WriteLine("Welcome to the mindfulness program. This program has 3 activities to help you punder and refelt during the day");
+    Console.WriteLine("Welcome to the mindfulness program. This program has 3 activities to help you ponder and reflectt during the day");
     
-        
+        int runProgram = 0;
+        while(runProgram != 4){
+            DisplayMenu();
+            // Read user imput 
+            string userSelection = Console.ReadLine();
+            //parse imput to int
+            int activity = Int16.Parse(userSelection);
+
+
+            switch(activity)
+            {
+            //Breathing activity
+            case 1:
+                Console.Clear();
+                Breathing breathing = new Breathing(0,"Breathing");
+                breathing.WelcomMesage();
+                breathing.displayDescription();
+                // store the duration in seconds of the activty 
+                int getDuration = breathing.getDuration();
+                breathing.pauseProgram(3);
+                breathing.Breath(getDuration);
+                
+            break;
+
+            //Reflecting activity
+            case 2:
+                Console.Clear();
+                Reflecting reflecting = new Reflecting(0,"Refelcting");
+                reflecting.WelcomMesage();
+                reflecting.displayDescription();
+                // store the duration in seconds of the activty 
+                getDuration = reflecting.getDuration();
+                reflecting.pauseProgram(3);
+                reflecting.reflect(getDuration);
+            break;
+            
+            //Reflecting listening
+            case 3:
+                Console.Clear();
+                Listening listening = new Listening(0,"Listening");
+                listening.WelcomMesage();
+                listening.displayDescription();
+                // store the duration in seconds of the activty 
+                getDuration = listening.getDuration();
+                listening.pauseProgram(3);
+                listening.listening(getDuration);
+            break;
+
+            //Stop the  program
+            case 4:
+                runProgram = 4;
+            break;
+            } 
+        }
 
     }
 }
