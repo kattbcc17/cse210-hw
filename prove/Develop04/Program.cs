480e2af6c80a243511b985c246045c208a9dcfe7
kattbcc17 <kattbcc17@byui.edu>
2023-06-01 11:47:13 -0500

    Develop 04

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Threading;
+
 
 class Program
-{
+{ 
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop04 World!");
+    {   
+        static void DisplayMenu(){
+        Console.Clear();    
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflecting activity");
+        Console.WriteLine("3. Start listening activity");
+        Console.WriteLine("4. Quit");
+        Console.Write("Select a choise from the menu: ");
+        }
+    
+    //Start
+    Console.Clear();
+    Console.WriteLine("Welcome to the mindfulness program. This program has 3 activities to help you punder and refelt during the day");
+    
+        
+
     }
 }
