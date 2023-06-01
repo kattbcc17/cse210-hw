228dd4f5ad39b668656ec1bc2ba3fc17f39a1dee
kattbcc17 <kattbcc17@byui.edu>
2023-06-01 11:37:52 -0500

    Main program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,42 +1,73 @@
 using System;
+using System.IO;
+
 class Program
 {
     static void Main(string[] args)
     {
-        int menuNum = 0;
-        var writtenEntries = new DisplayEntries(){};
-            while (menuNum != 5){
-                Console.WriteLine("Welcome to the Journal!");
-                Console.WriteLine("Please Select one of the following options from 1-5");
-                Console.WriteLine("==========================");
-                Console.WriteLine("1. New Journal Entry");
-                Console.WriteLine("2. Display Written Entries");
-                Console.WriteLine("3. Load Previous Entries");
-                Console.WriteLine("4. Save Current Entries");
-                Console.WriteLine("5. Exit Program");
-                menuNum = int.Parse(Console.ReadLine());
-            
-                if (menuNum == 1){
-                    WritePrompt prompt1 = new WritePrompt(writtenEntries);
-                    string entry1 = prompt1._displayPrompt();
-                    Console.WriteLine(entry1);
-                    Console.WriteLine();
-                }else if(menuNum == 2){
-                    string combo1 = writtenEntries._displayCombo();
-                    Console.WriteLine(combo1);
-                }else if(menuNum == 3){
-                    Console.Write("Input file name to be loaded: ");
-                    string fileName = Console.ReadLine();
-                    var myLoadFile = new JournalFile(writtenEntries);
-                    myLoadFile._loadFile(fileName);
-                    Console.WriteLine($"{fileName}.txt loaded!");
-                }else if(menuNum == 4){
-                    Console.Write("Input file name to be saved: ");
-                    string fileName = Console.ReadLine();
-                    var mySaveFile = new JournalFile(writtenEntries);
-                    mySaveFile._saveFile(fileName);
-                    Console.WriteLine($"{fileName}.txt saved!");
-                }
+
+        Console.WriteLine(); 
+        Console.WriteLine("Hi There! Welcome to your Daily Journal Program");
+        Console.WriteLine();  
+        //Create a Journal
+        Journal AJournal = new Journal();
+
+        //File loaded by the user
+        AJournal._loadedFile ="entries.txt";
+
+        // Set the loop 
+        int userResponse = 0;
+       
+
+        do{
+            Menu menu = new Menu();
+            menu.displayMenu(AJournal._loadedFile);
+            userResponse = int.Parse(Console.ReadLine());
+
+            // Option 1 Display a promp a prompt from the user and save it in an instance
+            if(userResponse == 1){
+              menu.Write(AJournal._loadedFile);
             }
+             // Display all the Entries from the instance
+            else if(userResponse == 2)
+            {
+              AJournal.DisplayEntries(AJournal._loadedFile);
+            }
+            //load and display entries from another file 
+            else if(userResponse == 3)
+            {
+              Console.WriteLine();
+              Console.WriteLine("1. Create new file.");
+              Console.WriteLine("2. Load file.");
+              Console.Write("Type an option: ");
+              int selection = Int32.Parse(Console.ReadLine());
+
+              if(selection == 1){
+                 Console.Write("Name of the new file: ");
+                 string fileName = Console.ReadLine(); 
+                 using (StreamWriter sw = File.CreateText(fileName))
+                  {
+                  sw.WriteLine($"----------------- -------------------");
+                  }	
+                  // load the file created 
+                  AJournal._loadedFile = fileName;
+
+              }else
+              {
+                Console.Write("Name of the file to load: ");
+                
+                // load the file enteres by the user 
+                AJournal._loadedFile = Console.ReadLine(); 
+              }
+              
+            }
+            else if(userResponse == 4)
+            {
+            //Create a file with the user entries
+              Console.WriteLine("Program ended, bye and hope to see you,!");
+
+            }
+
+        } while(userResponse != 4);     
     }
 }
